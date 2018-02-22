0f43905168ac7e345618ed7631c3c6f39c365042
rann <rannon888@gmail.com>
2018-02-22 09:08:00 +0700

    edit uer and position

--- database/migrations/2017_11_22_175734_create_positions_table.php
+++ database/migrations/2017_11_22_175734_create_positions_table.php
@@ -17,6 +17,7 @@ class CreatePositionsTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('description')->nullable();
+            $table->tinyInteger('active');
             $table->string('user_id');
             $table->timestamps();
         });
