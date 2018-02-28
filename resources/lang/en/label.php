d19e403275c1d28e66c147758a0733ccf718b752
rann <rannon888@gmail.com>
2018-02-28 06:53:04 -0800

    pull

--- resources/lang/en/label.php
+++ resources/lang/en/label.php
@@ -1,7 +1,6 @@
 <?php
     return [
 
-<<<<<<< HEAD
         'administrator' => 'Administrator',
         'add_new' => 'Add New',
         'user' => 'User',
@@ -15,24 +14,10 @@
         'action' => 'Action',
         'edit' => 'Edit',
         'create' => 'Create',
-        'cancel' => 'Cancel'
-=======
+        'cancel' => 'Cancel',
         'logout'            =>'Log Out',
-        'administrator'     => 'Administrator',
-        'add_new'           => 'Add New',
-        'user'              => 'User',
-        'position'          => 'Position',
-        'language'          => 'Language',
-        'language_code'     => 'Language Code',
-        'language_name'     => 'Language Name',
-        'language_list'     => 'Language List',
-        'No'                => 'No',
-        'created_by'        => 'Created By',
-        'action'            => 'Action',
-        'edit'              => 'Edit',
         'article'           => 'Article',
         'view'              =>'View'
->>>>>>> 0f634559b389a9e9a09c129250861d2989daebe5
     ]
 
 
--- resources/lang/kh/label.php
+++ resources/lang/kh/label.php
@@ -9,24 +9,16 @@
         'language'      => 'ភាសា',
         'language_code' => 'កូដភាសា',
         'language_name' => 'ឈ្មោះភាសា',
-<<<<<<< HEAD
         'table_language' => 'តារាងភាសា',
         'No' => 'ល.រ',
         'created_by' => 'បង្កើតដោយ',
         'action' => 'សកម្មភាព',
         'edit' => 'កែប្រែ',
         'create' => 'បង្កើត',
-        'cancel' => 'បោះបង់'
-=======
+        'cancel' => 'បោះបង់',
         'language_list' => 'បញ្ជីភាសា',
-        'No'            => 'ល.រ',
-        'created_by'    => 'បង្កើតដោយ',
-        'action'        => 'សកម្មភាព',
-        'edit'          => 'កែប្រែ',
         'article'       => 'អត្តបទ',
         'view'          => 'ទិដ្ឋភាព',
-
->>>>>>> 0f634559b389a9e9a09c129250861d2989daebe5
     ]
 
 
