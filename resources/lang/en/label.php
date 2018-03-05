a4ec6bf0e9bccb4ff26a4606e2b11a1ca1405375
sroun <31054168+sroun@users.noreply.github.com>
2018-03-05 18:57:55 +0700

    Update label.php

--- resources/lang/en/label.php
+++ resources/lang/en/label.php
@@ -17,7 +17,6 @@
         'cancel' => 'Cancel',
         'logout'            =>'Log Out',
         'article'           => 'Article',
-<<<<<<< HEAD
         'view'              =>'View',
         'update' => 'Update',
         'close' => 'Close',
@@ -38,9 +37,7 @@
         'choose_item' => 'Choose one',
         'name' => 'Name',
         'parent' => 'Parent'
-=======
         'view'              =>'View'
->>>>>>> 6230acf475a8c033c63d2e1b736821211a31cd1b
     ]
 
 
